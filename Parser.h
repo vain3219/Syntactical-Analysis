b662d445a7ae617e51744e92660b2d0b9277a44a
cThomp <cody5887@yahoo.com>
2020-04-05 00:01:36 -0700

    Add files via upload

--- Parser.h
+++ Parser.h
@@ -18,65 +18,69 @@ public:
     Parser(Lexer lex);
     
 private:
-    bool SyntaxAnalysis(std::vector< std::pair< std::string, std::string >> statement);
-    bool DeclarationRule(std::vector< std::pair< std::string, std::string >> statement);
-    bool AssignRule(std::vector< std::pair< std::string, std::string >> statement);
-    bool ExpressionRule();
+    bool SyntaxAnalysis( std::vector< std::pair< std::string, std::string >> statement );
+    bool DeclarationRule( std::vector< std::pair< std::string, std::string >> statement );
+    bool AssignRule( std::vector< std::pair< std::string, std::string >> statement );
+    bool ExpressionRule( std::vector< std::pair< std::string, std::string >> statement, int i );
     bool TermRule();
     bool FactorRule();
 };
 
 
-Parser::Parser(Lexer lex) {
+Parser::Parser( Lexer lex ) {
     std::vector< std::pair< std::string, std::string >> statement;
     std::pair<std::string, std::string> tmp;
     do {
         do {
             tmp = lex.getNextLexeme();                                  // get the next lexeme (pair of lexeme and token)
-            statement.push_back(tmp);                                   // push into the queue
-        } while( std::count(sep.begin(), sep.end(), tmp.first) );       // check if lexeme is a separator ? break out of while; get next lexeme
+            statement.push_back( tmp );                                   // push into the queue
+        } while( std::count( sep.begin(), sep.end(), tmp.first ) );       // check if lexeme is a separator ? break out of while; get next lexeme
         if( !SyntaxAnalysis(statement) )
             std::cerr << "Syntactical Error\n";
     } while( !lex.isEmpty() );                                          // loop until syntactical error is found or we've reached the end of the lexer queue
 }
 
-bool Parser::SyntaxAnalysis(std::vector< std::pair< std::string, std::string >> statement) {
-    DeclarationRule(statement);
+bool Parser::SyntaxAnalysis( std::vector< std::pair< std::string, std::string >> statement ) {
     
-    AssignRule(statement);
-    
-    ExpressionRule();
-    
-    TermRule();
-    
-    FactorRule();
-    
-    return true;
+    if( DeclarationRule(statement) ) {
+        return true;
+    } else if ( AssignRule(statement) ) {
+        return true;
+    } else if ( ExpressionRule(statement, 0) ) {
+        return true;
+    } else if ( TermRule() ) {
+        return true;
+    } else if ( FactorRule() ) {
+        return true;
+    }
+
+    return false;
 }
 
 bool Parser::DeclarationRule(std::vector< std::pair< std::string, std::string >> statement) {
     std::pair<std::string, std::string> tmp = statement.at(0);
-    
     if( std::count(sep.begin(), sep.end(), tmp.first) ) {
         tmp = statement.at(1);
-        if( tmp.second == "Identifier")
+        if( tmp.first == "Identifier")
             return true;
     }
-    
     return false;
 }
 
 bool Parser::AssignRule(std::vector<std::pair<std::string, std::string> > statement) {
     std::pair<std::string, std::string> tmp = statement.at(0);
-    
-    if(tmp.second == "Identifier") {
+    if( tmp.first == "Identifier" ) {
         tmp = statement.at(1);
-        if(tmp.first == "=") {
-            tmp = statement.at(2);
-            // call ExpressionRule()
+        if( tmp.second == "=" ) {
+            ExpressionRule( statement, 2 );
         }
     }
     return false;
 }
 
+bool Parser::ExpressionRule( std::vector< std::pair< std::string, std::string >> statement, int i ) {
+        std::pair<std::string, std::string> tmp = statement.at(i);
+    
+    return false;
+}
 #endif /* Parser_h */
