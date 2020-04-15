63d141085cc1e3425c515cc1842edf03c8768f03
cThomp <cody5887@yahoo.com>
2020-04-15 04:49:44 -0700

    In wokring condition
    grammars are validated correctly
    productions are printed out properly along with the token in question

--- Parser.h
+++ Parser.h
@@ -41,38 +41,37 @@ Parser::Parser( Lexer lex ) {
             statement.emplace_back( tmp );                                      // push into the queue
         } while( tmp.first != "Separator");                                     // check if lexeme is a separator ? break out of while; get next lexeme
         statement.pop_back();                                                   // delete the separator ( ';' is not included in any productions )
-        statement.emplace_back( std::make_pair("$", "$") );                     // add the '$' to the end of the statement to signify the end
+        statement.emplace_back( std::make_pair("$", "$") );                     // add the '$' to the end of the statement to signify the statements end
         for(int i = 0; i < statement.size(); i++) {                             // print the statement being processed
             tmp = statement.at(i);
             std::cout << tmp.second << " ";
         }
-        std::cout << "\n-------------" << std::endl;
-        if( !SyntaxAnalysis(statement) )                                        // begin the analysis
-            std::cerr << "Syntactical Error\n\n";                               // illegal
-        else
-            std::cout << "No error detected\n\n";                               // legal
+        std::cout << "\n---------------------";
+        std::cout << "\n| tkn | Productions |";
+        std::cout << "\n---------------------\n";
+        if( SyntaxAnalysis(statement) )                                         // begin the analysis
+            std::cout << "No error detected\n\n";
         statement.clear();                                                      // clear statement vector
     } while( !lex.isEmpty() );                                                  // loop until syntactical error is found or we've reached the end of the lexer queue
 }
 
 bool Parser::SyntaxAnalysis( std::vector< std::pair< std::string, std::string > > statement ) {
     if( DeclarationRule( statement ) ) {                                        // check declaration grammar
-        std::cout << " \tS -> D\n";
         return true;
     } else if ( AssignRule( statement ) ) {                                     // check assignation grammar
-        std::cout << " \tS -> A\n";
         return true;
     }
-    
+    std::cerr << "\tSyntax Error\n\n";
     return false;
 }
 
 bool Parser::DeclarationRule(std::vector< std::pair< std::string, std::string > > statement) {
     std::pair<std::string, std::string> tmp = statement.at(0);
+    std::cout << tmp.second + "\t\tS -> D\n";
     if( std::count(type.begin(), type.end(), tmp.second) ) {                    // First(D)
         tmp = statement.at(1);
         if( tmp.first == "Identifier")
-            std::cout << tmp.second + "\tD -> Type id\n";
+            std::cout << tmp.second + "\t\tD -> Type id\n";
         return true;
     }
     return false;
@@ -80,73 +79,79 @@ bool Parser::DeclarationRule(std::vector< std::pair< std::string, std::string > 
 
 bool Parser::AssignRule(std::vector<std::pair<std::string, std::string> > statement ) {
     std::pair<std::string, std::string> tmp = statement.at(i);
+    std::cout << tmp.second + " \t\tS -> A\n";
     if( tmp.first == "Identifier" ) {                                           // First(A)
         tmp = statement.at(++i);
         if( tmp.second == "=" ) {
+            std::cout << tmp.second + "\t\tA -> id = E\n";
             i++;
             if( ExpressionRule( statement ) )                                   // check expression grammar
-                std::cout << tmp.second + "\tA -> id = E\n";
             return true;
         }
     }
+    std::cerr << "\tAssignation rule error\n";
     return false;
 }
 
 bool Parser::ExpressionRule( std::vector< std::pair< std::string, std::string > > statement ) {
+    std::cout << " \t\tE -> TE'\n";
     if( TermRule( statement ) ) {                                               // First(E)
         if( ExpPrime( statement ) ) {
-            std::cout << " \tE -> TE'\n";
             return true;
         }
     }
+    std::cerr << "\tExpression rule error\n";
     return false;
 }
 
 bool Parser::ExpPrime( std::vector< std::pair< std::string, std::string > > statement ) {
     std::pair<std::string, std::string> tmp = statement.at(i);
     if( tmp.second == "+" || tmp.second == "-" ) {
+        std::cout << tmp.second + "\t\tE' -> +TE' | -TE'\n";
         i++;
         if( TermRule( statement ) ) {                                           // First(E')
             if( ExpPrime( statement ) ) {
-                std::cout << tmp.second + "\tE' -> +TE' | -TE'\n";
                 return true;
             }
         }
     } else {                                                                    // Follow(E')
         if( tmp.second == ")" || tmp.second == "$" ) {
-            std::cout << tmp.second + "\tE' -> e\n";
+            std::cout << tmp.second + "\t\tE' -> e\n";
             return true;
         }
     }
+    std::cerr << "\tExpression Prime rule error\n";
     return false;
 }
 
 bool Parser::TermRule( std::vector< std::pair< std::string, std::string > > statement ) {
+    std::cout << " \t\tT -> FT'\n";
     if( FactorRule( statement ) ) {
         if( TermPrime( statement ) ) {                                          // First(T)
-            std::cout << " \tT -> FT'\n";
             return true;
         }
     }
+    std::cerr << "\tTerm rule error\n";
     return false;
 }
 
 bool Parser::TermPrime( std::vector< std::pair< std::string, std::string > > statement ) {
     std::pair<std::string, std::string> tmp = statement.at(i);
     if( tmp.second == "*" || tmp.second == "/" ) {                              // First(T')
+        std::cout << tmp.second + "\t\tT' -> *FT' | /FT'\n";
         i++;
         if( FactorRule( statement ) ) {
             if( TermPrime( statement ) ) {
-                std::cout << tmp.second + "\tT' -> *FT' | /FT'\n";
                 return true;
             }
         }
     } else {                                                                    // Follow(T')
         if( tmp.second == ")" || tmp.second == "+" || tmp.second == "-" || tmp.second == "$" ) {
-            std::cout << tmp.second + "\tT' -> e\n";
+            std::cout << tmp.second + "\t\tT' -> e\n";
             return true;
         }
     }
+    std::cerr << "\tTerm Prime rule error\n";
     return false;
 }
 
@@ -157,18 +162,19 @@ bool Parser::FactorRule( std::vector< std::pair< std::string, std::string > > st
         if( ExpressionRule( statement ) ) {
             tmp = statement.at(i);
             if( tmp.second == ")" )
-                std::cout << tmp.second + "\tF -> (E)\n";
+                std::cout << tmp.second + "\t\tF -> (E)\n";
             return true;
         }
     } else if( tmp.first == "Identifier" ) {
         i++;
-        std::cout << tmp.second + "\tF -> id\n";
+        std::cout << tmp.second + "\t\tF -> id\n";
         return true;
     } else if( tmp.first == "Integer" ) {
         i++;
-        std::cout << tmp.second + "\tF -> num\n";
+        std::cout << tmp.second + "\t\tF -> num\n";
         return true;
     }
+    std::cerr << "\tFactor rule error\n";
     return false;
 }
 
