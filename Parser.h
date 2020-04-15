badd4e78e452d54f4a102eaa321fb411aff45efb
cThomp <cody5887@yahoo.com>
2020-04-15 04:20:20 -0700

    successfully validates grammar
    all 4 cases in myfile.txt have been successfully validated 
    all we need to do now is to format the production strings properly

--- Parser.h
+++ Parser.h
@@ -20,14 +20,14 @@ public:
 private:
     bool SyntaxAnalysis( std::vector< std::pair< std::string, std::string > > statement );
     bool DeclarationRule( std::vector< std::pair< std::string, std::string > > statement );
-    bool AssignRule( std::vector< std::pair< std::string, std::string > > statement, int &i );
-    bool ExpressionRule( std::vector< std::pair< std::string, std::string > > statement, int &i );
-    bool ExpPrime( std::vector< std::pair< std::string, std::string > > statement, int &i );
-    bool TermRule( std::vector< std::pair< std::string, std::string > > statement, int &i );
-    bool TermPrime( std::vector< std::pair< std::string, std::string > > statement, int &i );
-    bool FactorRule( std::vector< std::pair< std::string, std::string > > statement, int &i );
+    bool AssignRule( std::vector< std::pair< std::string, std::string > > statement );
+    bool ExpressionRule( std::vector< std::pair< std::string, std::string > > statement );
+    bool ExpPrime( std::vector< std::pair< std::string, std::string > > statement );
+    bool TermRule( std::vector< std::pair< std::string, std::string > > statement );
+    bool TermPrime( std::vector< std::pair< std::string, std::string > > statement );
+    bool FactorRule( std::vector< std::pair< std::string, std::string > > statement );
     void PrintProductions();
-    std::stack<std::string> productions;
+    int i;
 };
 
 
@@ -35,6 +35,7 @@ Parser::Parser( Lexer lex ) {
     std::vector< std::pair< std::string, std::string > > statement;
     std::pair<std::string, std::string> tmp;
     do {
+        i = 0;
         do {
             tmp = lex.getNextLexeme();                                          // get the next lexeme (pair of lexeme and token)
             statement.emplace_back( tmp );                                      // push into the queue
@@ -45,146 +46,130 @@ Parser::Parser( Lexer lex ) {
             tmp = statement.at(i);
             std::cout << tmp.second << " ";
         }
+        std::cout << "\n-------------" << std::endl;
         if( !SyntaxAnalysis(statement) )                                        // begin the analysis
-            std::cerr << "\nSyntactical Error\n";                               // illegal
+            std::cerr << "Syntactical Error\n\n";                               // illegal
         else
-            std::cout << "\nNo error detected\n";                               // legal
+            std::cout << "No error detected\n\n";                               // legal
         statement.clear();                                                      // clear statement vector
-        PrintProductions();
     } while( !lex.isEmpty() );                                                  // loop until syntactical error is found or we've reached the end of the lexer queue
 }
 
 bool Parser::SyntaxAnalysis( std::vector< std::pair< std::string, std::string > > statement ) {
-    int i = 0;
-    
     if( DeclarationRule( statement ) ) {                                        // check declaration grammar
-        productions.push("S -> D\n");
-    } else if ( AssignRule( statement, i ) ) {                                  // check assignation grammar
-        productions.push("S -> A\n");
+        std::cout << " \tS -> D\n";
+        return true;
+    } else if ( AssignRule( statement ) ) {                                     // check assignation grammar
+        std::cout << " \tS -> A\n";
+        return true;
     }
     
     return false;
 }
 
-bool Parser::DeclarationRule(std::vector< std::pair< std::string, std::string >> statement) {
+bool Parser::DeclarationRule(std::vector< std::pair< std::string, std::string > > statement) {
     std::pair<std::string, std::string> tmp = statement.at(0);
     if( std::count(type.begin(), type.end(), tmp.second) ) {                    // First(D)
         tmp = statement.at(1);
         if( tmp.first == "Identifier")
-            productions.push("D -> Type id\n");
+            std::cout << tmp.second + "\tD -> Type id\n";
         return true;
     }
     return false;
 }
 
-bool Parser::AssignRule(std::vector<std::pair<std::string, std::string> > statement, int &i ) {
-    std::pair<std::string, std::string> tmp = statement.at(i++);
-    int x = i;
+bool Parser::AssignRule(std::vector<std::pair<std::string, std::string> > statement ) {
+    std::pair<std::string, std::string> tmp = statement.at(i);
     if( tmp.first == "Identifier" ) {                                           // First(A)
-        tmp = statement.at(i++);
+        tmp = statement.at(++i);
         if( tmp.second == "=" ) {
-            if( ExpressionRule( statement, i ) )                                // check expression grammar
-                productions.push("A -> id = E\n");
+            i++;
+            if( ExpressionRule( statement ) )                                   // check expression grammar
+                std::cout << tmp.second + "\tA -> id = E\n";
             return true;
         }
     }
-    i = x;
     return false;
 }
 
-bool Parser::ExpressionRule( std::vector< std::pair< std::string, std::string > > statement, int &i ) {
+bool Parser::ExpressionRule( std::vector< std::pair< std::string, std::string > > statement ) {
+    if( TermRule( statement ) ) {                                               // First(E)
+        if( ExpPrime( statement ) ) {
+            std::cout << " \tE -> TE'\n";
+            return true;
+        }
+    }
+    return false;
+}
+
+bool Parser::ExpPrime( std::vector< std::pair< std::string, std::string > > statement ) {
     std::pair<std::string, std::string> tmp = statement.at(i);
-    int x = i;
-    if( TermRule( statement, i ) ) {                                            // First(E)
-        if( ExpPrime( statement, i ) ) {
-            productions.push("E -> TE'\n");
-            return true;
-        }
-    }
-    i = x;
-    return false;
-}
-
-bool Parser::ExpPrime( std::vector< std::pair< std::string, std::string > > statement, int &i ) {
-    std::pair<std::string, std::string> tmp = statement.at(i++);
-    int x = i;
     if( tmp.second == "+" || tmp.second == "-" ) {
-        if( TermRule( statement, i ) ) {                              // First(E')
-            if( ExpPrime( statement, i ) ) {
-                productions.push("E' -> +TE' | -TE'\n");
+        i++;
+        if( TermRule( statement ) ) {                                           // First(E')
+            if( ExpPrime( statement ) ) {
+                std::cout << tmp.second + "\tE' -> +TE' | -TE'\n";
                 return true;
             }
         }
-    } else {                                                        // Follow(E')
-        if( tmp.second == ")" || tmp.second == "$" ) {              
-            productions.push("E' -> e\n");
+    } else {                                                                    // Follow(E')
+        if( tmp.second == ")" || tmp.second == "$" ) {
+            std::cout << tmp.second + "\tE' -> e\n";
             return true;
         }
     }
-    i = x;
     return false;
 }
 
-bool Parser::TermRule( std::vector< std::pair< std::string, std::string > > statement, int &i ) {
-    int x = i;
-    if( FactorRule( statement, i ) ) {
-        if( TermPrime( statement, i ) ) {                             // First(T)
-            productions.push("T -> FT'\n");
+bool Parser::TermRule( std::vector< std::pair< std::string, std::string > > statement ) {
+    if( FactorRule( statement ) ) {
+        if( TermPrime( statement ) ) {                                          // First(T)
+            std::cout << " \tT -> FT'\n";
             return true;
         }
     }
-    i = x;
     return false;
 }
 
-bool Parser::TermPrime( std::vector< std::pair< std::string, std::string > > statement, int &i ) {
+bool Parser::TermPrime( std::vector< std::pair< std::string, std::string > > statement ) {
     std::pair<std::string, std::string> tmp = statement.at(i);
-    int x = i;
-    if( tmp.second == "*" || tmp.second == "/" ) {                  // First(T')
-        if( FactorRule( statement, i ) ) {
-            if( TermPrime( statement, i ) ) {
-                productions.push("T' -> *FT' | /FT'\n");
+    if( tmp.second == "*" || tmp.second == "/" ) {                              // First(T')
+        i++;
+        if( FactorRule( statement ) ) {
+            if( TermPrime( statement ) ) {
+                std::cout << tmp.second + "\tT' -> *FT' | /FT'\n";
                 return true;
             }
         }
-    } else {                                                        // Follow(T')
+    } else {                                                                    // Follow(T')
         if( tmp.second == ")" || tmp.second == "+" || tmp.second == "-" || tmp.second == "$" ) {
-            productions.push("T' -> e\n");
+            std::cout << tmp.second + "\tT' -> e\n";
             return true;
         }
     }
-    i = x;
     return false;
 }
 
-bool Parser::FactorRule( std::vector< std::pair< std::string, std::string > > statement, int &i ) {
-    std::pair<std::string, std::string> tmp = statement.at(i++);\
-    int x = i;
-    if( tmp.second == "(" ) {                                       // First(F)
-        if( ExpressionRule( statement, i ) ) {
-            tmp = statement.at(i++);
+bool Parser::FactorRule( std::vector< std::pair< std::string, std::string > > statement ) {
+    std::pair<std::string, std::string> tmp = statement.at(i);
+    if( tmp.second == "(" ) {
+        i++;
+        if( ExpressionRule( statement ) ) {
+            tmp = statement.at(i);
             if( tmp.second == ")" )
-                productions.push("F -> (E)\n");
+                std::cout << tmp.second + "\tF -> (E)\n";
             return true;
         }
     } else if( tmp.first == "Identifier" ) {
-        productions.push("F -> id\n");
+        i++;
+        std::cout << tmp.second + "\tF -> id\n";
         return true;
     } else if( tmp.first == "Integer" ) {
-        productions.push("F -> num\n");
+        i++;
+        std::cout << tmp.second + "\tF -> num\n";
         return true;
     }
-    i = x;
     return false;
 }
 
-void Parser::PrintProductions() {
-    std::string e;
-    while( !productions.empty() ) {
-        e = productions.top();
-        std::cout << e;
-        productions.pop();
-    }
-    std::cout << std::endl;
-}
 #endif /* Parser_h */
